0ff37824233d3774acceb17ff756e4e119dd8e78
fdlessard <fdlessard@gmail.com>
2018-02-21 22:00:13 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/reactive/ReactiveCustomerApplication.java
@@ -0,0 +1,12 @@
+package io.fdlessard.codebites.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCustomerApplication.class, args);
+	}
+}
